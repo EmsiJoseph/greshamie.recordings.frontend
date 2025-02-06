cf03c4ae9d467b4cb6973aa7b21845caa6781f34
Jamzy <jamzymarietc@gmail.com>
2025-02-06 10:41:43 +0800

    Refactor authentication interfaces and schemas; enhance login action implementation

--- lib/interfaces/authentication-interfaces.ts
+++ lib/interfaces/authentication-interfaces.ts
@@ -2,13 +2,17 @@ import { IBaseApiResponse } from "./base-api-response-interface";
 import { IUser, IUserWithToken } from "./user-interfaces";
 
 export interface IAuthCookie extends IUserWithToken {
-    // "user_name": string,
-    // "access_token": string,
-    // "expires_in": number,
+    user_name: string;
+    access_token: string;
+    expires_in: number;
 }
 
 export interface ILoginApiResponse extends IBaseApiResponse<IUserWithToken> {
-    // "user_name": string,
-    // "access_token": string,
-    // "expires_in": number,
+    user_name: string;
+    access_token: string;
+    expires_in: number;
+}
+
+export interface ILoginOutput{
+    message: string;
 }
--- lib/services/server-actions/authentication.ts
+++ lib/services/server-actions/authentication.ts
@@ -1,74 +1,80 @@
-// "use server";
+"use server";
 
-// import { AxiosResponse } from "axios";
-// import { ILoginApiResponse } from "@/lib/interfaces/authentication-interfaces";
-// import { flattenValidationErrors } from "next-safe-action";
-// import { GreshamAxiosConfig } from "@/lib/config/main-backend-axios-config";
-// import { loginEndpoint } from "@/api/endpoints/auth-endpoints";
-// import { LoginSchema } from "@/lib/schema/authentication-schema";
-// import { actionClient } from "@/lib/config/safe-action";
-// // import { loginUserUseCase } from "@/core/use-cases/users";
-// import { cookies } from "next/headers";
-// // import { loginRoute } from "@/config/api/backend-routes/auth-routes";
-// import { handleUseServerResponse } from "@/lib/handlers/api-response-handlers/handle-use-server-response";
-// import { setAuthCookie } from "@/lib/services/server-actions/cookie";
+import { AxiosResponse } from "axios";
+import { ILoginApiResponse } from "@/lib/interfaces/authentication-interfaces";
+import { flattenValidationErrors } from "next-safe-action";
+import { GreshamAxiosConfig } from "@/lib/config/main-backend-axios-config";
+import { loginEndpoint } from "@/api/endpoints/auth-endpoints";
+import { LoginSchema } from "@/lib/schema/authentication-schema";
+import { cookies } from "next/headers";
+import { actionClient } from "@/lib/config/safe-action";
+import { setAuthCookie } from "./cookie";
+import { handleUseServerResponse } from "@/lib/handlers/api-response-handlers/handle-use-server-response";
+// import { loginUserUseCase } from "@/core/use-cases/users";
+// import { loginRoute } from "@/config/api/backend-routes/auth-routes";
 
-// // TODO: Implement zsa
-// /**
-//  * Handles login action.
-//  *
-//  * @remarks
-//  * This action is used to handle login request from the client.
-//  * It uses the {@link loginUserUseCase} use case to perform the business logic for logging in.
-//  *
-//  * @param {LoginInput} input - The login input.
-//  * @returns {Promise<LoginOutput>} - The login output.
-//  */
-// export const loginUserAction = actionClient
-//     .schema(LoginSchema, {
-//         handleValidationErrorsShape: async (ve) =>
-//             flattenValidationErrors(ve).fieldErrors,
-//     })
-//     .action(
-//         async ({
-//             parsedInput: { email, password},
-//         }) => {
-//             const request = async (): Promise<
-//                 AxiosResponse<ILoginApiResponse>> => {
-//                 let requestBody: { email: string; password: string; } = {
-//                     email,
-//                     password,
-//                 };
+// TODO: Implement zsa
+/**
+ * Handles login action.
+ *
+ * @remarks
+ * This action is used to handle login request from the client.
+ * It uses the {@link loginUserUseCase} use case to perform the business logic for logging in.
+ *
+ * @param {LoginInput} input - The login input.
+ * @returns {Promise<LoginOutput>} - The login output.
+ */
+export const loginUserAction = actionClient
+    .schema(LoginSchema, {
+        handleValidationErrorsShape: async (ve) =>
+            flattenValidationErrors(ve).fieldErrors,
+    })
+    .action(
+        async ({
+            parsedInput: { email, password },
+        }) => {
+            const request = async (): Promise<
+                AxiosResponse<ILoginApiResponse>> => {
+                let requestBody: { email: string; password: string} = {
+                    email,
+                    password,
+                };
 
-//                 return GreshamAxiosConfig.post<ILoginApiResponse>(
-//                     loginEndpoint,
-//                     requestBody
-//                 );
-//             };
+                return GreshamAxiosConfig.post<ILoginApiResponse>(
+                    loginEndpoint,
+                    requestBody
+                );
+            };
 
-//             // Handle the response
-//             const response = await handleUseServerResponse({
-//                 request,
-//                 successMessage: "Logged in successfully! 🎉",
-//             });
-            
+            // Handle the response
+            const response = await handleUseServerResponse({
+                request,
+                successMessage: "Logged in successfully! 🎉",
+            });
+            // if (response?.data?.status) {
+            //     const isSetSuccessfully = await setAuthCookie(response?.data);
+            //     if (!isSetSuccessfully) {
+            //         throw new Error("Failed to set auth cookie");
+            //     }
+            // }
 
-//             return response;
-//         }
-//     );
+            return response;
+        }
+    );
 
-// export const logoutUserAction = actionClient.action(async () => {
-//     return new Promise((resolve, reject) => {
-//         try {
-//             const cookiesToDelete = cookies().getAll();
-//             cookiesToDelete.forEach((cookie) => {
-//                 cookies().delete(cookie.name);
-//             });
-//             setTimeout(() => {
-//                 resolve(!cookies().has("auth"));
-//             }, 100);
-//         } catch (error) {
-//             reject(error);
-//         }
-//     });
-// });
+export const logoutUserAction = actionClient.action(async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const allCookies = await cookies();
+            const cookiesToDelete = allCookies.getAll();
+            cookiesToDelete.forEach((cookie) => {
+                allCookies.delete(cookie.name);
+            });
+            setTimeout(() => {
+                resolve(!allCookies.has("auth"));
+            }, 100);
+        } catch (error) {
+            reject(error);
+        }
+    });
+});
