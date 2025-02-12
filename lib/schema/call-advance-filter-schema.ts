37bb6627af60af46e4efe6cd96aef9b7fec120be
John Christian Berbon <john.berbon@wizard-ai.com>
2025-02-12 15:49:47 +0800

    Added time in the call-advance-filter-schema because date and time is separated

--- lib/schema/call-advance-filter-schema.ts
+++ lib/schema/call-advance-filter-schema.ts
@@ -1,3 +1,4 @@
+import { start } from "node:repl";
 import { date, z } from "zod";
 
 export const CallAdvanceFilterSchema = z
@@ -10,6 +11,19 @@ export const CallAdvanceFilterSchema = z
         endDate: date()
             .refine((date) => date <= new Date(), {
                 message: "End date should not be in the future.",
+            })
+            .optional(),
+        // Time format should be HH:mm (24-hour time format)
+        startTime: z
+            .string()
+            .regex(/^(?:[01]\d|2[0-3]):(?:[0-5]\d)$/, {
+                message: 'Start time should be in HH:mm (24-hour) format.',
+            })
+            .optional(),
+        endTime: z
+            .string()
+            .regex(/^(?:[01]\d|2[0-3]):(?:[0-5]\d)$/, {
+                message: 'End time should be in HH:mm (24-hour) format.',
             })
             .optional(),
         minimumDurationSeconds: z.preprocess(
@@ -30,18 +44,64 @@ export const CallAdvanceFilterSchema = z
         hasPciCompliance: z.boolean().optional(),
         hasQualityEvaluation: z.boolean().optional(),
     })
+    // --> 01 Has time but no Dates
     .refine(
         (data) => {
-            if (data.startDate && data.endDate) {
-                return data.startDate <= data.endDate;
+            if (!data.startDate && data.startTime) {
+                return false;
             }
             return true;
         },
         {
-            message: "Start date should not be greater than end date.",
-            path: ["startDate"], // This sets the error on the startDate field
+            message: "Start date is required when start time is not empty.",
+            path: ["startDate"],  // Error will be set on the startDate field
         }
     )
+    .refine(
+        (data) => {
+            if (!data.endDate && data.endTime) {
+                return false;
+            }
+            return true;
+        },
+        {
+            message: "End date is required when end time is not empty.",
+            path: ["endDate"],
+        }
+    )
+    // ---> 03 Check Date time range [conditionally add the time if present]
+    .refine(
+        (data) => {
+            const startDate = data.startDate;
+            const endDate = data.endDate
+            const startTime = data.startTime
+            const endTime = data.endTime
+
+            if (startDate) {
+                if (startTime) {
+                    const [startHours, startMinutes] = startTime.split(":").map(Number);  // Convert to numbers from string regex
+                    startDate.setHours(startHours, startMinutes, 0, 0) // Set hours, minutes, seconds, milliseconds
+                }
+            }
+
+            if (endDate) {
+                if (endTime) {
+                    const [endHours, endMinutes] = endTime.split(":").map(Number);
+                    endDate.setHours(endHours, endMinutes, 0, 0)
+                }
+            }
+
+            if (startDate && endDate) {
+                return startDate <= endDate;
+            }
+            return true;
+        },
+        {
+            message: "Start date-time should not be greater than end date-time.",
+            path: ["startDate"],
+        }
+    )
+    // --> Check duration range
     .refine(
         (data) => {
             if (data.minimumDurationSeconds !== undefined && data.maximumDurationSeconds !== undefined) {
