65bdaba0520d6ea8aa5228491294421d282ee05a
Mc Joseph Agbanlog <Mcjoseph.Agbanlog@wizard-ai.com>
2025-02-14 15:51:36 +0800

    Add reverse proxy configuration and services for synchronization and blob storage

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'standalone',
-  
+    reactStrictMode: true,
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '1mb', // Set your desired limit
+            allowedOrigins: ['http://localhost:3000'], // Configure origins as needed
+        },  // Ensure this is enabled
+    },
 };
 
 export default nextConfig;
