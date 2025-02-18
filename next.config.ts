6c362494ee74f72c680552f911fbadca02bc4b8b
Mc Joseph Agbanlog <Mcjoseph.Agbanlog@wizard-ai.com>
2025-02-18 10:16:51 +0800

    Update CORS configuration, add reverse proxy settings, and enhance authentication handling

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const clientAppUri = process.env.NEXT_PUBLIC_CLIENT_APP_URI || "http://localhost:3000";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    distDir: "build",
-    output: "standalone"
+    // reactStrictMode: true,
+    // distDir: "build",
+    // output: "standalone"
 };
 
 export default nextConfig;
